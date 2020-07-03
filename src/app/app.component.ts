b3f594b8ec4b81ee0a1bac152d568cbb084185b6
tomaszbabinski <tommybab@gmail.com>
2020-07-03 16:59:18 +0200

    Added advanced transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,30 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)  '
+      })),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 
 export class AppComponent {
+  wildState = 'normal';
   state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
@@ -35,6 +52,11 @@ export class AppComponent {
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
 
